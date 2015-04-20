797e70ff27f96aadc28f1a8fbe52ddeb2beaff1f
Jackie D'Elia <loneduchess@gmail.com>
2015-04-20 11:08:40 -0400

    Update genesis-site-title-styles.php

--- genesis-site-title-styles.php
+++ genesis-site-title-styles.php
@@ -13,7 +13,7 @@
  * Plugin Name:       Genesis Site Title Styles
  * Plugin URI:        https://github.com/savvyjackie/genesis-site-title-styles
  * Description:       Adds a span tag to each word in the site title for separate styling with css using the nth-child() selector.
- * Version:           1.0
+ * Version:           1.01
  * Author:            Jackie D'Elia and Ginger Coolidge
  * Author URI:        http://www.savvyjackiedesigns.com
  * Text Domain:       genesis-site-title-styles
@@ -24,6 +24,20 @@
  * GitHub Branch:     master
  */
 
+/*  
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 
 // If this file is called directly, abort.
 if ( ! defined( 'ABSPATH' ) ) {
